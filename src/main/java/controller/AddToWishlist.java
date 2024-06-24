1b2f7cdee9e3e891d0fea20403d755c4eb9eb7f9
AndreaPanza18 <164524326+AndreaPanza18@users.noreply.github.com>
2024-06-24 15:07:36 +0200

    Update AddToWishlist.java

--- src/main/java/controller/AddToWishlist.java
+++ src/main/java/controller/AddToWishlist.java
@@ -1,11 +1,12 @@
 package controller;
-
 import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import jakarta.servlet.*;
 import model.*;
 import java.util.*;
+
+//This servlet controlls if the User is logged in and let them add games into the wishlist
 @WebServlet("/AddToWishlist")
 public class AddToWishlist extends HttpServlet{
     @Override
