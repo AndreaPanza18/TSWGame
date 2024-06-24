1d9e98c8e709d4f590cac4a239fd5a859787695b
AndreaPanza18 <164524326+AndreaPanza18@users.noreply.github.com>
2024-06-24 15:08:45 +0200

    Update GetTrending.java

--- src/main/java/controller/GetTrending.java
+++ src/main/java/controller/GetTrending.java
@@ -1,5 +1,4 @@
 package controller;
-
 import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
@@ -7,15 +6,14 @@ import jakarta.servlet.*;
 import model.*;
 import java.util.*;
 
+//This servlet find all the treanding games
 @WebServlet("/GetTrending")
-
 public class GetTrending extends HttpServlet  {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         GameDOA gameSearch = new GameDOA();
         HttpSession ssn = request.getSession(true);
         List<Game> games = gameSearch.getTrending();
-
 
         ssn.setAttribute("games", games);
         RequestDispatcher view = request.getRequestDispatcher("home-page.jsp");
